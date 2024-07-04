410fab5ccd1d3664d5628977cccb86cad24598c7
Panayiotis Halouvas <phalouvas@gmail.com>
2024-07-04 05:12:03 +0000

    chore: Update version to 1.1.0 and inform user if supplier have possible unlined entries

---
+++ optimusland/utils/supplier.py
@@ -0,0 +1,36 @@
+import frappe
+from frappe import _, qb, throw
+
+@frappe.whitelist()
+def get_supplier_unlinked_journal_entries(supplier_name: str):
+	
+	supplier_jv = frappe.db.sql(f"""
+		SELECT 
+			T1.`parent`
+		FROM `tabJournal Entry Account` AS `T1`
+		WHERE T1.`party` = '{supplier_name}'
+			AND T1.`party_type` = 'Supplier'
+			AND T1.`docstatus` < 2;
+	""", as_dict=False)
+
+	customer_jv = frappe.db.sql(f"""
+		SELECT 
+			T1.`parent`
+		FROM `tabJournal Entry Account` AS `T1`
+		WHERE T1.`party` = '{supplier_name}'
+			AND T1.`party_type` = 'Customer'
+			AND T1.`reference_type` IS NULL
+			AND T1.`reference_name` IS NULL
+			AND T1.`docstatus` < 2
+			AND T1.`parent` IN ({','.join([f"'{row[0]}'" for row in supplier_jv])});
+	""", as_dict=True)
+
+	if customer_jv:
+		non_empty_parents = [row['parent'] for row in customer_jv if row['parent']]
+		message = f"Possible false balance. <br/>There are unlinked journal entries for supplier {supplier_name} with Voucher No:<br/> {', '.join(non_empty_parents)}"
+		#frappe.msgprint(message, title="Warning")
+		return message
+	
+	return True
+	
+	pass
