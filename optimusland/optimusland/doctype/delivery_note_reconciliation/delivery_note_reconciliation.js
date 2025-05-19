3eb5d54546ab257ce412a3bf904c425ead1d9f50
Panayiotis Halouvas <phalouvas@gmail.com>
2025-05-19 07:24:41 +0000

    Remove Delivery Note Reconciliation and Allocation DocTypes along with associated files

--- optimusland/optimusland/doctype/delivery_note_reconciliation/delivery_note_reconciliation.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, KAINOTOMO PH LTD and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Delivery Note Reconciliation", {
-// 	refresh(frm) {
-
-// 	},
-// });
