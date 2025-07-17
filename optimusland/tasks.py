6ed6db95ec2e4090533f78307856f082bfbc737f
Panayiotis Halouvas <phalouvas@gmail.com>
2025-07-17 04:33:22 +0000

    feat: update validation for purchase receipts to warn about potatoes without batch numbers

--- optimusland/tasks.py
+++ optimusland/tasks.py
@@ -1,9 +1,9 @@
 import frappe
 
-def daily():
-    frappe.enqueue("optimusland.utils.invoices_status.fix_unpaid_overdue_purchase_invoices_status")
-    frappe.enqueue("optimusland.utils.invoices_status.fix_unpaid_overdue_sales_invoices_status")
-    frappe.enqueue("optimusland.utils.invoices_status.fix_to_bill_delivery_note_status")
+#def daily():
+#    frappe.enqueue("optimusland.utils.invoices_status.fix_unpaid_overdue_purchase_invoices_status")
+#    frappe.enqueue("optimusland.utils.invoices_status.fix_unpaid_overdue_sales_invoices_status")
+#    frappe.enqueue("optimusland.utils.invoices_status.fix_to_bill_delivery_note_status")
 
 # You can also define other scheduled tasks here
 # def all():
