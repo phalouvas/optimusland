5171771fc3db85c8defd3bf89b89ec92664bf292
Panayiotis Halouvas <phalouvas@gmail.com>
2025-07-17 04:14:54 +0000

    feat: update version to 3.4.1 and modify sales invoice hooks to warn about unlinked items

--- optimusland/hooks.py
+++ optimusland/hooks.py
@@ -136,7 +136,7 @@ doc_events = {
         "validate": "optimusland.utils.purchase_receipt.set_batch_no",
     },
     "Sales Invoice": {
-        "validate": "optimusland.utils.sales_invoice.get_delivery_note_items"
+        "before_save": "optimusland.utils.sales_invoice.warn_unlinked_items"
     },
     "Purchase Invoice": {
         "validate": "optimusland.utils.purchase_invoice.get_purchase_receipt_items"
@@ -150,9 +150,9 @@ scheduler_events = {
 # 	"all": [
 # 		"optimusland.tasks.all"
 # 	],
- 	"daily": [
- 		"optimusland.tasks.daily"
- 	],
+    "daily": [
+        "optimusland.tasks.daily"
+    ],
 # 	"hourly": [
 # 		"optimusland.tasks.hourly"
 # 	],
--- optimusland/utils/sales_invoice.py
+++ optimusland/utils/sales_invoice.py
@@ -9,38 +9,16 @@ def mark_paid(invoice_name: str):
     """, ("Paid", invoice_name))
     return True
 
-def get_delivery_note_items(sales_invoice, method=None):
-
-    items_delivery_notes = [
-        item.delivery_note
-        for item in sales_invoice.items
-        if item.delivery_note
-    ]
-
-    items = [
-        item
-        for item in sales_invoice.items
-        if not item.delivery_note
-    ]
-
-    if not items:
-        return
-    
-    for item in items:
-        delivery_note_items = frappe.db.sql("""
-            SELECT dni.name, dni.parent
-            FROM `tabDelivery Note Item` dni
-            INNER JOIN `tabDelivery Note` dn ON dni.parent = dn.name
-            WHERE dn.customer = %s 
-            AND dn.docstatus = 1
-            AND dn.status = 'To Bill'
-            AND dni.item_code = %s
-            AND dni.parent NOT IN %s
-            ORDER BY dn.posting_date
-            LIMIT 1
-        """, values=(sales_invoice.customer, item.item_code, tuple(items_delivery_notes) if items_delivery_notes else ("",)), as_dict=1)
-        if not delivery_note_items:
-            continue
-        item.delivery_note = delivery_note_items[0].parent
-        item.dn_detail = delivery_note_items[0].name
+def warn_unlinked_items(sales_invoice, method=None):
+    unlinked_items = [item for item in sales_invoice.items if not item.delivery_note]
+    if unlinked_items:
+        item_list = "<ul>" + "".join([
+            f"<li>{getattr(item, 'item_code', 'Unknown Item')} (Row #{getattr(item, 'idx', '?')})</li>"
+            for item in unlinked_items
+        ]) + "</ul>"
+        frappe.msgprint(
+            "Warning: The following items in this Sales Invoice are not linked to a Delivery Note:" + item_list,
+            title="Unlinked Items",
+            indicator="orange"
+        )
         
