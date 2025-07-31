b3b8ba30743324f2056b6e13b03c4931357239af
Panayiotis Halouvas <phalouvas@gmail.com>
2025-07-31 12:09:21 +0000

    feat: enhance Purchase Receipt and Weight Slip with new fields and improved item sorting

--- optimusland/public/js/purchase_receipt.js
+++ optimusland/public/js/purchase_receipt.js
@@ -6,17 +6,25 @@ frappe.ui.form.on('Purchase Receipt', {
             frm.doc.items.sort(function(a, b) {
                 return (b.qty || 0) - (a.qty || 0);
             });
-            
             // Update idx field to maintain the order after save
             frm.doc.items.forEach(function(item, index) {
                 item.idx = index + 1;
             });
-            
             // Mark the document as dirty to ensure changes are saved
             frm.dirty();
-            
             // Refresh the items table to reflect the new order
             frm.refresh_field('items');
         }
+    },
+
+    refresh: function(frm) {
+        frm.set_query('custom_weight_slip', function() {
+            return {
+                filters: {
+                    supplier: frm.doc.supplier || '',
+                    disabled: 0
+                }
+            };
+        });
     }
 });
