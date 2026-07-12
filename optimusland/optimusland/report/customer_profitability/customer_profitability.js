de4f3afc9a90d41f36a7cbec1c59e18404b4e0a1
Panayiotis Halouvas <phalouvas@gmail.com>
2026-07-12 11:40:08 +0000

    profitability reports

---
+++ optimusland/optimusland/report/customer_profitability/customer_profitability.js
@@ -0,0 +1,56 @@
+// Copyright (c) 2026, KAINOTOMO PH LTD and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Customer Profitability"] = {
+	"filters": [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1,
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			reqd: 1,
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+		},
+		{
+			fieldname: "customer",
+			label: __("Customer"),
+			fieldtype: "Link",
+			options: "Customer",
+		},
+		{
+			fieldname: "supplier",
+			label: __("Supplier"),
+			fieldtype: "Link",
+			options: "Supplier",
+		},
+	],
+	"formatter": function(value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (data && data.customer === "Total") {
+			value = "<b>" + value + "</b>";
+		}
+		if (column.fieldname === "margin_achieved" && data && data.margin_achieved != null && data.customer !== "Total") {
+			var color = data.margin_achieved >= 0 ? "green" : "red";
+			value = "<span style='color:" + color + ";font-weight:bold'>" + value + "</span>";
+		}
+		if (column.fieldname === "profit_amount" && data && data.profit_amount != null && data.customer !== "Total") {
+			var color = data.profit_amount >= 0 ? "green" : "red";
+			value = "<span style='color:" + color + ";font-weight:bold'>" + value + "</span>";
+		}
+		return value;
+	}
+};
