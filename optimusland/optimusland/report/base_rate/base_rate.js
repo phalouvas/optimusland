a3cb50da0d3cd3ea72390abbc5365e7361333c72
Panayiotis Halouvas <phalouvas@gmail.com>
2026-07-12 04:52:50 +0000

    added base rate report

---
+++ optimusland/optimusland/report/base_rate/base_rate.js
@@ -0,0 +1,62 @@
+// Copyright (c) 2026, KAINOTOMO PH LTD and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Base Rate"] = {
+	"filters": [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1,
+		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			reqd: 1,
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+		},
+		{
+			fieldname: "supplier",
+			label: __("Grower (Supplier)"),
+			fieldtype: "Link",
+			options: "Supplier",
+		},
+		{
+			fieldname: "customer",
+			label: __("Customer"),
+			fieldtype: "Link",
+			options: "Customer",
+		},
+		{
+			fieldname: "batch_no",
+			label: __("Batch No"),
+			fieldtype: "Data",
+		},
+		{
+			fieldname: "sales_invoice",
+			label: __("Sales Invoice"),
+			fieldtype: "Link",
+			options: "Sales Invoice",
+		},
+	],
+	"formatter": function(value, row, column, data, default_formatter) {
+		value = default_formatter(value, row, column, data);
+		if (column.fieldname == "formula_price" && data && data.formula_price && data.actual_price) {
+			var diff = data.actual_price - data.formula_price;
+			if (Math.abs(diff) > 0.001) {
+				value = '<span style="color:orange">' + value + '</span>';
+			}
+		}
+		return value;
+	}
+};
